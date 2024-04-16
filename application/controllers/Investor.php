02b1d6bd8de8e32791b1cfe1bd08f71ac6f499b4
shankha <shankhacse@gmail.com>
2024-04-16 19:33:05 +0530

    investor relations

--- application/controllers/Investor.php
+++ application/controllers/Investor.php
@@ -5,17 +5,67 @@ class Investor extends CI_Controller {
         parent::__construct();
         
     }
-
-
     public function index(){
         $session = $this->session->userdata('user_detail');
 		if($this->session->userdata('user_detail'))
 		{ 
             $page = "dashboard/investor_relations/investor_relations_view";
             $header="";    
-            $result = "";   
+            $result = [];   
           
             createbody_method($result, $page, $header, $session);
+        }else{
+			redirect('login','refresh');
+		}
+    }
+
+
+    public function corporate_governance_partial_view(){
+        $session = $this->session->userdata('user_detail');
+		if($this->session->userdata('user_detail'))
+		{ 
+            
+            $orderby='relations_dtl_id';
+            $result['relationsDetailsList'] = $this->commondatamodel->getAllRecordWhereOrderBy('investor_relations_details',[],$orderby);   
+          // pre($result['relationsDetailsList']);
+            $page = "dashboard/investor_relations/corporate_governance_partial_view";
+            $this->load->view($page,$result);
+        }else{
+			redirect('login','refresh');
+		}
+    }
+
+    public function shareholders_information_partial_view(){
+        $session = $this->session->userdata('user_detail');
+		if($this->session->userdata('user_detail'))
+		{ 
+            $result = [];  
+            $page = "dashboard/investor_relations/shareholders_information_partial_view";
+            $this->load->view($page,$result);
+        }else{
+			redirect('login','refresh');
+		}
+    }
+
+    public function financials_partial_view(){
+        $session = $this->session->userdata('user_detail');
+		if($this->session->userdata('user_detail'))
+		{ 
+            $result = [];  
+            $page = "dashboard/investor_relations/financials_partial_view";
+            $this->load->view($page,$result);
+        }else{
+			redirect('login','refresh');
+		}
+    }
+
+    public function financials_partial_notice(){
+        $session = $this->session->userdata('user_detail');
+		if($this->session->userdata('user_detail'))
+		{ 
+            $result = [];  
+            $page = "dashboard/investor_relations/financials_partial_notice";
+            $this->load->view($page,$result);
         }else{
 			redirect('login','refresh');
 		}
--- assets-admin/js/customJs/investor_relations.js
+++ assets-admin/js/customJs/investor_relations.js
@@ -2,22 +2,22 @@
 
 $(document).ready(function () {
     var basepath = $("#basepath").val();
-    //loadPartialView("#tab_one", basepath + "media/video_partial_view");
+    loadPartialView("#tab_one", basepath + "investor/corporate_governance_partial_view");
     $("#tab_one-tab").on("click", function(event) {
         event.preventDefault();
-        loadPartialView("#tab_one", basepath + "media/video_partial_view");
+        loadPartialView("#tab_one", basepath + "investor/corporate_governance_partial_view");
     });
     $("#tab_two-tab").on("click", function(event) {
         event.preventDefault();
-        loadPartialView("#tab_two", basepath + "media/news_partial_view");
+        loadPartialView("#tab_two", basepath + "investor/shareholders_information_partial_view");
     });
     $("#tab_three-tab").on("click", function(event) {
         event.preventDefault();
-        loadPartialView("#tab_three", basepath + "media/events_happining_partial_view");
+        loadPartialView("#tab_three", basepath + "investor/financials_partial_view");
     });
     $("#tab_four-tab").on("click", function(event) {
         event.preventDefault();
-        loadPartialView("#tab_four", basepath + "media/newslater_partal_view");
+        loadPartialView("#tab_four", basepath + "investor/financials_partial_notice");
     });
     $(".update_btn").on("click", function(event) {
         event.preventDefault();
@@ -35,11 +35,15 @@ $(document).ready(function () {
     
 }); // end of document ready
 function loadPartialView(tabId, partialViewUrl) {
+
+    
+    $("#tab_one_data,#tab_two_data,#tab_three_data,#tab_four_data").html("");
     $.ajax({
         url: partialViewUrl,
         type: 'GET',
         success: function(data) {
             $(tabId + "_data").html(data);
+            $('.dataTable').DataTable({ paging: false });
         },
         error: function(xhr, status, error) {
             console.error("Error loading partial view:", error);
